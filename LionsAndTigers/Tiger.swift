8f057fbb53fa713820e2ae441ebef259ff280c08
ryanpixilink <ryan@pixilink.com>
2014-12-23 16:26:41 -0800

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Ryan Liu on 2014-12-23.
+//  Copyright (c) 2014 Ryan Liu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0;
+    var name = "";
+    var breed = "";
+    var image = UIImage(named:"");
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger();
+        myTiger.name = "Tigger";
+        myTiger.breed = "Bengal";
+        myTiger.age = 3;
+        myTiger.image = UIImage(named: "BengalTiger.jpg");
+        
+        myImageView.image = myTiger.image;
+        nameLabel.text = myTiger.name;
+        ageLabel.text = "\(myTiger.age)";
+        breedLabel.text = myTiger.breed;
+        
+        var secondTiger = Tiger();
+        secondTiger.name = "Tigress";
+        secondTiger.breed = "Indochinese Tiger";
+        secondTiger.age = 2;
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg");
+        
+        var thirdTiger = Tiger();
+        thirdTiger.name = "Jacob";
+        thirdTiger.breed = "Malayan Tiger";
+        thirdTiger.age = 4;
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg");
+        
+        var fourthTiger = Tiger();
+        fourthTiger.name = "Spar";
+        fourthTiger.breed = "Siberian Tiger";
+        fourthTiger.age = 5;
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg");
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
 }
 
