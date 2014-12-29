d2d7e0869e69c3a5228a058571bae999e6fa516f
ryanpixilink <ryan@pixilink.com>
2014-12-29 11:23:30 -0800

    added random facts about tigers

--- LionsAndTigers/Tiger.swift
+++ LionsAndTigers/Tiger.swift
@@ -14,4 +14,45 @@ struct Tiger {
     var name = "";
     var breed = "";
     var image = UIImage(named:"");
+    
+    func chuff() {
+        println("Tiger: Chuff Chuff");
+    }
+    
+    func chufANumberOfTimes (numberOfTime: Int) {
+        for var chuff = 0; chuff < numberOfTime; ++chuff {
+            self.chuff();
+        }
+    }
+    
+    func chuffANumberOfTimes (numberOfTimes: Int, isLoud: Bool) {
+        for var chuffTimes = 1; chuffTimes <= numberOfTimes; chuffTimes++ {
+            if isLoud {
+                chuff();
+            } else {
+                println("Tiger: purr purr");
+            }
+        }
+    }
+    
+    func ageInTigerYearsFromAge(regularAge : Int) -> Int {
+        let newAge = regularAge * 3;
+        return newAge;
+    }
+    
+    func randomFact() -> String {
+        let randomNumber = Int(arc4random_uniform(UInt32(3)));
+        
+        var randomFact:String;
+        
+        if randomNumber == 0 {
+            randomFact = "The Tiger is the biggest species in the cat family";
+        } else if randomNumber == 1 {
+            randomFact = "Tigers can reach a lenght of 3.3 meters";
+        } else {
+            randomFact = "a group of tigers is known as an 'ambush' or 'strea'";
+        }
+        
+        return randomFact;
+    }
 }
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var randomFactLabel: UILabel!
 
+    var myTigers:[Tiger] = [];
+    var currentIndex = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +29,14 @@ class ViewController: UIViewController {
         myTiger.age = 3;
         myTiger.image = UIImage(named: "BengalTiger.jpg");
         
-        myImageView.image = myTiger.image;
-        nameLabel.text = myTiger.name;
-        ageLabel.text = "\(myTiger.age)";
-        breedLabel.text = myTiger.breed;
+        myTiger.age = myTiger.ageInTigerYearsFromAge(myTiger.age);
+        myTiger.chuffANumberOfTimes(5, isLoud: true);
+        
+        self.myImageView.image = myTiger.image;
+        self.nameLabel.text = myTiger.name;
+        self.ageLabel.text = "\(myTiger.age)";
+        self.breedLabel.text = myTiger.breed;
+        self.randomFactLabel.text = myTiger.randomFact();
         
         var secondTiger = Tiger();
         secondTiger.name = "Tigress";
@@ -49,6 +56,7 @@ class ViewController: UIViewController {
         fourthTiger.age = 5;
         fourthTiger.image = UIImage(named: "SiberianTiger.jpg");
         
+        myTigers += [myTiger, secondTiger, thirdTiger, fourthTiger];
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,6 +65,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        var randomIndex:Int;
+        
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)));
+        } while currentIndex == randomIndex
+        
+        currentIndex = randomIndex;
+        
+        let tiger = myTigers[randomIndex];
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            self.myImageView.image = tiger.image;
+            self.nameLabel.text = tiger.name;
+            self.ageLabel.text = "\(tiger.age)";
+            self.breedLabel.text = tiger.breed;
+            self.randomFactLabel.text = tiger.randomFact();
+            }, completion: {
+                (finished: Bool) -> () in
+        })
     }
 }
 
